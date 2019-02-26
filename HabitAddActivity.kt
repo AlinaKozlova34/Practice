db2c9fa6dd6a1220cc2080abbe5210a6e3283284
Alina Kozlova <art-line.lina@yandex.ru>
2019-02-27 02:54:35 +0300

    HabitActivity modify interface

--- HabitAddActivity.kt
+++ HabitAddActivity.kt
@@ -2,6 +2,9 @@ package padev.badhabits.application.ui.activity
 
 import android.os.Bundle
 import android.support.v7.widget.Toolbar
+import android.view.View
+import android.widget.RadioButton
+import android.widget.TextView
 import padev.badhabits.R
 import padev.badhabits.core.view.BaseActivity
 
@@ -14,8 +17,17 @@ class HabitAddActivity: BaseActivity() {
 
         val toolbar = findViewById<Toolbar>(R.id.activity_habit_add_appbar_toolbar)
         setSupportActionBar(toolbar)
+
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         supportActionBar?.title = getString(R.string.habit_add)
+
+        val unnecessaryThingsRadioButton = findViewById<RadioButton>(R.id.activity_habit_add_radio_button_unnecessary_things)
+
+        unnecessaryThingsRadioButton.setOnCheckedChangeListener { buttonView, isChecked ->
+            val unnecessaryThingsLongTextView = findViewById<TextView>(R.id.activity_habit_add_text_view_unnecessary_things_long)
+            if (isChecked) unnecessaryThingsLongTextView.visibility = View.VISIBLE
+            else unnecessaryThingsLongTextView.visibility = View.GONE
+        }
     }
 }
